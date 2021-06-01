d784aa1817c4f973c0d0b176de3593c73eb0aa27
wangpeng14 <WpyyBen1a2s3d>
2021-06-01 17:34:24 +0800

    is A BiTree equals B BiTree.

--- src/test/kotlin/com/wangpeng/learning/algorithm/bitree/BiTreeTest.kt
+++ src/test/kotlin/com/wangpeng/learning/algorithm/bitree/BiTreeTest.kt
@@ -6,10 +6,36 @@ import org.junit.jupiter.api.extension.ExtendWith
 
 @ExtendWith(MockKExtension::class)
 class BiTreeTest {
+	/**
+	 * 判断两个二叉树是否相同
+	 */
+	@Test
+	fun isEqualBiTree() {
+		val biTree1 = mockBiTree(intArrayOf(1, 2, 3, 4, 5, 8, 9, 10))
+		val biTree2 = mockBiTree(intArrayOf(1, 2, 3, 4, 5, 8, 9, 10))
+		println("biTree1 is equal biTree2 " + isEqual(biTree1, biTree2))
+		val biTree3 = mockBiTree(intArrayOf(1, 2, 3, 4, 5, 8, 9, 10, 18))
+		println("biTree1 is equal biTree3 " + isEqual(biTree1, biTree3))
+	}
+	
+	private fun isEqual(standard: BiTree?, compare: BiTree?): Boolean {
+		if (standard == null && compare == null) return true
+		if (standard != null && compare == null) return false
+		if (standard == null && compare != null) return false
+		return if (standard?.data == compare?.data) {
+			isEqual(standard?.leftChild, compare?.leftChild)
+					&& isEqual(standard?.rightChild, compare?.rightChild)
+		} else false
+	}
+	
+	private fun mockBiTree(array: IntArray): BiTree? {
+		return toBiTree(array, 0, array.size - 1)
+	}
+	
 	
 	@Test
 	fun changeArrayToBiTree() {
-		val arr = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9,10)
+		val arr = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 		val biTree = toBiTree(arr, 0, arr.size - 1)
 		printBiTree(biTree)
 	}
